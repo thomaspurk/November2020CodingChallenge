7939f16f1407423ec99fc16009bb36a6c7d8ef23
thomaspurk <thomas.purk@gastechnology.org>
2020-12-15 22:59:51 -0600

    For 12/15 coding club

---
+++ scripts/leafletjs.js
@@ -0,0 +1,117 @@
+//debugger;
+var mymap = L.map("mapid").setView([0, 0], 1);
+
+function getColor(d) {
+  return d > 7
+    ? "#800026"
+    : d > 6
+    ? "#BD0026"
+    : d > 5
+    ? "#E31A1C"
+    : d > 3
+    ? "#FC4E2A"
+    : d > 3
+    ? "#FD8D3C"
+    : d > 2
+    ? "#FEB24C"
+    : d > 1
+    ? "#FED976"
+    : "#FFEDA0";
+}
+
+function style(feature) {
+  return {
+    fillColor: getColor(feature.properties.map_color8),
+    weight: 2,
+    opacity: 1,
+    color: "white",
+    dashArray: "3",
+    fillOpacity: 0.3
+  };
+}
+
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+    weight: 5,
+    color: "#666",
+    dashArray: "",
+    fillOpacity: 0.3
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+    layer.bringToFront();
+  }
+
+  window.clocks = [];
+  window.clocks.push(layer.feature.properties.tz_name1st);
+}
+
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+}
+
+var geojson;
+// ... our listeners
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: zoomToFeature
+  });
+}
+
+geojson = L.geoJSON(geoJSON_TimeZones, {
+  style: style,
+  onEachFeature: onEachFeature
+}).addTo(mymap);
+
+function zoomToFeature(e) {
+  map.fitBounds(e.target.getBounds());
+}
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+  maxZoom: 18,
+  id: "mapbox/streets-v11",
+  tileSize: 512,
+  zoomOffset: -1
+  // accessToken: "your.mapbox.access.token"
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+var circle = L.circle([51.508, -0.11], {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 0.5,
+  radius: 500
+}).addTo(mymap);
+
+var polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(mymap);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+  .setLatLng([51.5, -0.09])
+  .setContent("I am a standalone popup.")
+  .openOn(mymap);
+
+//var popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(mymap);
+}
+
+mymap.on("click", onMapClick);
---
+++ scripts/time.js
@@ -0,0 +1,55 @@
+// IANA Time Zones https://www.iana.org/time-zones
+// https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+window.clocks = [
+  // "America/New_York",
+  "Europe/London"
+  // "Europe/Moscow",
+  // "Pacific/Tahiti",
+  // "Pacific/Fiji",
+  // "America/Phoenix"
+];
+
+let displayTime = function (args) {
+  // Get the existing Document Object Model (DOM) element to add clocks to.
+  // Clear the DOM element
+  // Get the current Date and Time
+  let divTime = document.getElementById("time");
+  divTime.innerHTML = "";
+  let curTime = new Date(Date.now());
+
+  // Loop each clock Time Zone and create the DOM elements that make up the clock
+  for (let i = 0; i < window.clocks.length; i++) {
+    // Convert curren time based o the IANA Time Zone Code
+    let thisTime = "No IANA Time Zone Code";
+    try {
+      thisTime = curTime.toLocaleString("en-US", {
+        timeZone: window.clocks[i]
+      });
+    } catch (e) {
+      thisTime = e.message;
+    }
+    let divClock = document.createElement("div");
+    let spanZone = document.createElement("span");
+    let spanDate = document.createElement("span");
+    let spanDigits = document.createElement("span");
+
+    // Control styles with a styles by class names
+    divClock.classList.add("clock");
+    spanDigits.classList.add("digits");
+
+    // Set the content
+    spanZone.innerHTML = window.clocks[i];
+    spanDigits.innerHTML = thisTime.split(",")[1];
+    spanDate.innerHTML = thisTime.split(",")[0];
+
+    // Append the new DOM elements to the HTML page
+    divClock.append(...[spanZone, spanDate, spanDigits]);
+    divTime.appendChild(divClock);
+  }
+
+  // Use the page rendering refresh event to
+  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+  window.requestAnimationFrame(displayTime);
+};
+
+window.requestAnimationFrame(displayTime);
